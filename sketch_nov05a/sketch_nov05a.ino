5adb7ab6e5d5d92cb7dd1f3271b9c5629a78add9
mTwardowski020 <56068022+mTwardowski020@users.noreply.github.com>
2020-11-05 18:16:23 +0100

    Initial commit

---
+++ sketch_nov05a/sketch_nov05a.ino
@@ -0,0 +1,17 @@
+/*
+ * Simple 1.77" TFT 8P Color Display ST7735s 128x160
+ * 
+ * 
+ * Author: Marek Twardowski / mTwardowski020
+ * 
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
